274120e7af2ef56ca0c17cef395c826668520863
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-03-23 10:11:11 -0400

    Update terminal1.bash

--- terminal1.bash
+++ terminal1.bash
@@ -1,9 +1,7 @@
 
 husky000@bandit:~$ nc -lvp 4400
-listening on [any] 4426 ...
-connect to [192.168.56.3] from gangsta.inetsec.edu [192.168.56.4] 55964
-bin/grade
-/bin//sh: 1: bin/grade: not found
+listening on [any] 4400 ...
+connect to [***.***.**.*] from *****.inetsec.edu [***.***.**.*] *****
 /bin/grade
 Congratulations -- your hash is 000000000000000000000000000000:00000:00000:
 ^C
